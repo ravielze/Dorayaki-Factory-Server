e8ff0884c59da5fce602ea22599415173ec2917d
Jason Stanley <13519019@std.stei.itb.ac.id>
2021-11-26 00:54:02 +0000

    Jason

--- controller/inbound/index.ts
+++ controller/inbound/index.ts
@@ -1,22 +1,88 @@
 import { Request, Response } from 'express';
 import AsyncHandler from 'express-async-handler';
 import { Service } from 'typedi';
-import { CreateResponse, ResponseStatus } from '../../model/dto';
-import InboundService from '../../service/inbound';
-import { BaseController, Controller } from '../base';
+import { ParseIntOrDefault, ParseInt } from "../../common/parser"
+import { CreateResponse, ResponseStatus } from "../../model/dto"
+import InboundService from "../../service/inbound"
+import { BaseController, Controller } from "../base"
+import { PaginationResult } from "../../model/dto/abstract"
+import { ConvertInbound, InboundDTO } from "../../model/dto/inbound"
+import UserMiddleware from "../user/middleware"
+import { CreateInboundDTO } from "../../model/dto/inbound/create"
+import { validate } from "../../common/validation"
+import { InboundStatus } from "../../common/inboundStatus"
 
 @Service()
 class InboundController extends BaseController implements Controller {
-    basePath = '/inbound';
+  basePath = "/inbound"
 
-    constructor(private readonly service: InboundService) {
-        super();
-        this.router.get('/', AsyncHandler(this.something.bind(this)));
-    }
+  constructor(
+    private readonly service: InboundService,
+    private readonly mw: UserMiddleware
+  ) {
+    super()
+    this.router.get(
+      "/",
+      this.mw.hasAPIKeyOrIsAuthorized(),
+      AsyncHandler(this.getAll.bind(this))
+    )
+    this.router.post(
+      "/",
+      this.mw.hasAPIKeyOrIsAuthorized(),
+      AsyncHandler(this.createInbound.bind(this))
+    )
+    this.router.post(
+      "/receive/:id",
+      this.mw.hasAPIKeyOrIsAuthorized(),
+      AsyncHandler(this.receive.bind(this))
+    )
+  }
 
-    async something(req: Request, res: Response) {
-        res.json(CreateResponse(ResponseStatus.OK));
+  async something(req: Request, res: Response) {
+    res.json(CreateResponse(ResponseStatus.OK))
+  }
+
+  async createInbound(req: Request, res: Response) {
+    const item: CreateInboundDTO = new CreateInboundDTO(req.body)
+    await validate(item)
+
+    const result = await this.service.createInbound(req, item)
+    res.return(CreateResponse(ResponseStatus.OK, ConvertInbound(result)))
+  }
+
+  async getAll(req: Request, res: Response) {
+    const page: number = ParseIntOrDefault(
+      1,
+      req.query.page as string | undefined,
+      "Page"
+    )
+    const itemPerPage: number = ParseIntOrDefault(
+      10,
+      req.query.items_per_page as string | undefined,
+      "ItemsPerPage"
+    )
+    const result = await this.service.getInbounds(req, page, itemPerPage)
+    const resultDto: PaginationResult<InboundDTO> = {
+      ...result,
+      items: result.items.map((i) => ConvertInbound(i)),
     }
+    res.return(CreateResponse(ResponseStatus.OK, resultDto))
+  }
+
+  async receive(req: Request, res: Response) {
+    const id: number = ParseInt(
+      req.params.id as string | undefined,
+      "InboundID"
+    )
+
+    const result = await this.service.updateStatus(
+      req,
+      id,
+      InboundStatus.RECEIVED
+    )
+
+    res.return(CreateResponse(ResponseStatus.OK, ConvertInbound(result)))
+  }
 }
 
 export default InboundController;
--- service/inbound/index.ts
+++ service/inbound/index.ts
@@ -1,18 +1,59 @@
 import { Request } from 'express';
 import { Service } from 'typedi';
-import { PaginationResult } from '../../model/dto/abstract';
-import InboundRepository from '../../repository/inbound';
-import DorayakiService from '../dorayaki';
-import IngredientService from '../ingredient';
-import { InboundServiceError } from './error';
+import { InboundStatus } from "../../common/inboundStatus"
+import { InboundDAO } from "../../model/dao/inbound"
+import { PaginationResult } from "../../model/dto/abstract"
+import { CreateInboundDTO } from "../../model/dto/inbound/create"
+import InboundRepository from "../../repository/inbound"
+import DorayakiService from "../dorayaki"
+import IngredientService from "../ingredient"
+import { InboundServiceError } from "./error"
 
 @Service()
 class InboundService {
-    constructor(
-        private readonly repo: InboundRepository,
-        private readonly doraService: DorayakiService,
-        private readonly ingrService: IngredientService
-    ) {}
+  constructor(
+    private readonly repo: InboundRepository,
+    private readonly doraService: DorayakiService,
+    private readonly ingrService: IngredientService
+  ) {}
+  async createInbound(
+    req: Request,
+    item: CreateInboundDTO
+  ): Promise<InboundDAO> {
+    return this.repo.saveInbound(req, item.ToDAO())
+  }
+  async getInbounds(
+    req: Request,
+    page: number,
+    itemPerPage: number
+  ): Promise<PaginationResult<InboundDAO>> {
+    const result = await this.repo.getAllInbounds(req, page, itemPerPage)
+
+    if (page != 1 && page > result.maxPage) {
+      throw InboundServiceError.PAGE_NOT_FOUND
+    }
+
+    if (result.maxPage < 1) {
+      result.maxPage = 1
+    }
+
+    return result
+  }
+
+  async updateStatus(
+    req: Request,
+    id: number,
+    status: InboundStatus
+  ): Promise<InboundDAO> {
+    if (status === InboundStatus.RECEIVED) {
+      await this.repo.receive(req, id)
+    }
+    const result = await this.repo.getInbound(req, id)
+    if (!result) {
+      throw new Error("Imposible step deteceted")
+    }
+    return result
+  }
 }
 
 export default InboundService;
